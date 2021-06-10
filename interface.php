d422a67d5ee397307af8a20d8cf9b6c5690df396
Luidjy Aubel <luidjy.aubel@gmail.com>
2021-06-10 18:49:15 +0200

    adding selecting page (not complely working)

--- interface.php
+++ interface.php
@@ -9,6 +9,12 @@
       <form action="?CMD=NEW" method="post">
         <p>Nom de l'auteur</p><br/>
         <input type="text" name="auteur" placeholder="Nom de l'auteur"/><br/>
+        <p>selection de la page</p>
+        <select name="Page">
+        <option value="" select>--Please choose an option--</option>
+        <option value="blog">Blog</option>
+        <option value="veille">veille</option>
+        </select><br/>
         <p>Article</p><br/>
         <textarea name="text1" rows="9" cols="50" placeholder="Votre text ici !"></textarea><br/>
         <input type="submit" name="publier" value="Publier"/><br/>
@@ -17,9 +23,11 @@
         function article(){
         $auteur = $_POST['auteur'];
         $article = $_POST['text1'];
+        $Page = $_POST['Page'];
         setlocale(LC_TIME, 'fra_fra');
         $dateAr = strftime('%d/%m/%y, %H:%M');
-        echo "ce texte est écrit par $auteur, le $dateAr <br/><br/>";
+        echo "ce texte est écrit par $auteur, le $dateAr <br/>";
+        echo "votre article est publier sur la page $Page de votre site<br/><br/>";
         echo nl2br($article);
         //echo "$article";
         }
